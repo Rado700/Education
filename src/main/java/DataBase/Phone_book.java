e7ff0d567bd0c9f793b41d20672b9c6e73f7c59d
Radik <97843156+Rado700@users.noreply.github.com>
2024-02-13 21:54:51 +0300

    PostgresSQL add method

--- src/main/java/DataBase/Phone_book.java
+++ src/main/java/DataBase/Phone_book.java
@@ -1,6 +1,5 @@
 package DataBase;
 
-import java.security.Key;
 import java.sql.*;
 import java.sql.Date;
 import java.util.*;
@@ -22,10 +21,10 @@ public class Phone_book {
         }
     }
 
-    public void addUsers(String name, String surname,String url){
+    public void addUsers(String name, String surname, String url) {
 
-        try{
-            String adds = "INSERT INTO users (name,surname,url)"+
+        try {
+            String adds = "INSERT INTO users (name,surname,url)" +
                     "VALUES (?,?,?)";
             var pstmt = connection.prepareStatement(adds, Statement.RETURN_GENERATED_KEYS);
 
@@ -39,17 +38,18 @@ public class Phone_book {
             throw new RuntimeException(e);
         }
     }
-    public void addPosts(String contento, Date date, Time time, int users_id){
+
+    public void addPosts(String contento, Date date, Time time, int users_id) {
 
         try {
-            String addpst = "INSERT INTO post (contento,date,time,users_id)"+
+            String addpst = "INSERT INTO post (contento,date,time,users_id)" +
                     "VALUES (?,?,?,?)";
             var psmt = connection.prepareStatement(addpst, Statement.RETURN_GENERATED_KEYS);
 
-            psmt.setString(1,contento);
-            psmt.setDate(2,date);
-            psmt.setTime(3,time);
-            psmt.setInt(4,users_id);
+            psmt.setString(1, contento);
+            psmt.setDate(2, date);
+            psmt.setTime(3, time);
+            psmt.setInt(4, users_id);
 
             int insertedRow = psmt.executeUpdate();
 
@@ -57,127 +57,239 @@ public class Phone_book {
             throw new RuntimeException(e);
         }
     }
-    public void addLike(int person_post,int likes_post){
+
+    public void addLike(int post_id, int user_id) {
+
         try {
-            String addLikes = "INSERT INTO likes (person_post,likes_post)"+
+            String countLike = "SELECT name, post_id,count(user_id) FROM likes "+
+                    "INNER JOIN users on likes.user_id = users.id "+
+                    "GROUP BY name,post_id,user_id "+
+                    "HAVING post_id = "+ post_id+ " AND user_id ="+user_id;
+
+            var pst = connection.createStatement();
+            var rsPst = pst.executeQuery(countLike);
+
+            while (rsPst.next()){
+                String name = rsPst.getString("name");
+                int post = rsPst.getInt("post_id");
+                int like = rsPst.getInt("count");
+
+                if (like >= 1){
+                    System.out.println("пост уже имеет лайк");
+                    return;
+                }
+
+                System.out.println(name);
+                System.out.println(like);
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        try{
+            String myLike = "SELECT users_id FROM post"+
+                    " WHERE id = " + post_id;
+
+            var pstm = connection.createStatement();
+            var psw = pstm.executeQuery(myLike);
+
+            while (psw.next()){
+                int postUserId = psw.getInt("users_id");
+
+                if (postUserId == user_id){
+                    System.out.println("Нельзя лайкать свой пост");
+                }
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            String newLIKE = "INSERT INTO likes (post_id,user_id) "+
                     "VALUES (?,?)";
+            var stmtt= connection.prepareStatement(newLIKE,Statement.RETURN_GENERATED_KEYS);
 
-            var pstmnt = connection.prepareStatement(addLikes, Statement.RETURN_GENERATED_KEYS);
 
-            pstmnt.setInt(1,person_post);
-            pstmnt.setInt(2,likes_post);
+            stmtt.setInt(1,post_id);
+            stmtt.setInt(2,user_id);
 
-            int insertedRow = pstmnt.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public Map<String, List<String>> getlikePost(){
-
-        Map<String , List<String>>hashMap = new HashMap<>();
-
-        try {
-            String newLike = "SELECT name,likes_post,contento, person_post FROM users INNER JOIN post on users.id=post.id INNER JOIN likes on users.id=likes.person_post";
-            var stmn = connection.createStatement();
-            var rs = stmn.executeQuery(newLike);
-            while (rs.next()){
-                String name = rs.getString("name");
-                String contento = rs.getString("contento");
-                   int person_post = rs.getInt("person_post");
-
-                if (hashMap.containsKey("name")){
-                    hashMap.get(name).add(contento);
-                    hashMap.get(name).add(String.valueOf(person_post));
-                }else {
-                    List<String>newList = new ArrayList<>();
-                    newList.add(contento);
-                    hashMap.put(name,newList);
-                }
-            }
-
+            var ss = stmtt.executeUpdate();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return hashMap;
+
     }
+        public Map<String, Map<String, List<String>>> getlikePost () {
 
-    public Map<String, List<String>> getUsersPosts()  {
+            Map<String, Map<String, List<String>>> hashMap = new HashMap<>();
 
-        Map<String,List<String>> hashmap = new HashMap<>();
+            try {
+                String newLike = "SELECT author.name as author_name,contento, likes.user_id, visitor.name as visitor_name FROM likes " +
+                        "INNER JOIN post on likes.post_id=post.id " +
+                        "INNER JOIN users author on post.users_id=author.id " +
+                        "INNER JOIN users visitor on visitor.id=likes.user_id";
+                var stmn = connection.createStatement();
+                var rs = stmn.executeQuery(newLike);
+                while (rs.next()) {
+                    String author_name = rs.getString("author_name");
+                    String contento = rs.getString("contento");
+                    String user_id = rs.getString("user_id");
+                    String visitor = rs.getString("visitor_name");
 
-        try {
-            String query = "SELECT name,contento FROM users INNER JOIN post on users.id=post.users_id ";
-            var stmt = connection.createStatement();
-            var rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                String name = rs.getString("name");
-                String content = rs.getString("contento");
-
-                if (hashmap.containsKey(name)){
-                    hashmap.get(name).add(content);
-                } else {
-                    List<String>contentList = new ArrayList<>();
-                    contentList.add(content);
-                    hashmap.put(name,contentList);
+                    if (hashMap.containsKey(author_name)) {
+                        if (hashMap.get(author_name).containsKey(contento)) {
+                            hashMap.get(author_name).get(contento).add(visitor);
+                        } else {
+                            hashMap.get(author_name).put(contento, new ArrayList<>());
+                            hashMap.get(author_name).get(contento).add(visitor);
+                        }
+                    } else {
+                        Map<String, List<String>> hash = new HashMap<>();
+                        hash.put(contento, new ArrayList<>());
+                        hash.get(contento).add(visitor);
+                        hashMap.put(author_name, hash);
+                    }
                 }
+
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return hashMap;
+        }
+
+        public Map<String, List<String>> getUsersPosts () {
+
+            Map<String, List<String>> hashmap = new HashMap<>();
+
+            try {
+                String query = "SELECT name,contento FROM users INNER JOIN post on users.id=post.users_id ";
+                var stmt = connection.createStatement();
+                var rs = stmt.executeQuery(query);
+                while (rs.next()) {
+                    String name = rs.getString("name");
+                    String content = rs.getString("contento");
+
+                    if (hashmap.containsKey(name)) {
+                        hashmap.get(name).add(content);
+                    } else {
+                        List<String> contentList = new ArrayList<>();
+                        contentList.add(content);
+                        hashmap.put(name, contentList);
+                    }
+                }
+
+            } catch (SQLException sqlException) {
+                System.out.println("Ошибка запроса " + sqlException);
+            }
+            return hashmap;
+        }
+
+        public void deleteLike (int id){
+
+            try {
+                String name = "DELETE FROM likes WHERE id=?";
+                var sttm = connection.prepareStatement(name);
+
+                sttm.setInt(1, id);
+
+                int insertedRow = sttm.executeUpdate();
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
 
+
         }
-        catch (SQLException sqlException){
-            System.out.println("Ошибка запроса "+ sqlException);
-        }
-        return hashmap;
-    }
-    public void deleteString(int id){
+    public void deletePost (int id){
 
         try {
-            String name = "DELETE FROM likes WHERE id=?";
+            String name = "DELETE FROM post WHERE id=?";
             var sttm = connection.prepareStatement(name);
 
-            sttm.setInt(1,id);
+            sttm.setInt(1, id);
 
             int insertedRow = sttm.executeUpdate();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+
     }
 
 
-    public void getUserNames() throws SQLException {
+        public void getUserNames () throws SQLException {
 
-        String names = "SELECT * FROM users";
+            String names = "SELECT * FROM users";
 
 
-        var stmt = connection.createStatement();
+            var stmt = connection.createStatement();
 
-        var rs = stmt.executeQuery(names);
+            var rs = stmt.executeQuery(names);
 
-        while (rs.next()) {
-            System.out.println(rs.getString("name"));
-            System.out.println(rs.getString("url"));
-            System.out.println(rs.getInt("id"));
-            System.out.println(rs.getString("surname"));
-            System.out.println(rs.getInt("phonebook_id"));
-            System.out.println(rs.getBoolean("premium"));
+            while (rs.next()) {
+                System.out.println(rs.getString("name"));
+                System.out.println(rs.getString("url"));
+                System.out.println(rs.getInt("id"));
+                System.out.println(rs.getString("surname"));
+                System.out.println(rs.getInt("phonebook_id"));
+                System.out.println(rs.getBoolean("premium"));
+            }
+        }
+
+
+        public static void main (String[]args) throws SQLException {
+
+
+            Phone_book phone_book = new Phone_book();
+
+            phone_book.getlikePost();
+        //    phone_book.deletePost(8);
+            //         phone_book.addLike(3,1);
+            //       phone_book.addUsers("Sergei","Sergeev","http://vk5");
+            //          System.out.println(phone_book.getlikePost());
+            //       System.out.println(phone_book.getUsersPosts());
+            //      phone_book.addPosts("Happy new year", new Date(2024-1900,2,6),new Time(16,33,0),6);
+            //    phone_book.deleteLike(24);
         }
     }
+//> Рафаил:
+//Написать функцию, которая будет добавлять лайк на пост (параметры фунукции будут post_id, user_id).
+// Важно, если пользователь пытается лайкнуть свой же пост, не давать этого сделать.
+// Также не давать лайкать пользователю больше чем 1 раз
+//
+//> Рафаил:
+//Разбиваем на шаги.
+//1. SQL запрос, чтобы посчитать, сколько пользователь сделал уже лайков (COUNT())
+//2. SQL запрос, Проверить, не его ли это пост
+//3. Вставить свой лайк
 
 
-    public static void main(String[] args) throws SQLException {
+//            String addlike = "SELECT post_id,user_id,author.name as author_name,liker.name as liker_name  FROM likes " +
+//                    "INNER JOIN post on likes.user_id=post.id " +
+//                    "INNER JOIN users author on post.users_id=author.id " +
+//                    "INNER JOIN users liker on post.users_id=liker.id";
 
+//            String addLikes = "INSERT INTO likes (post_id,user_id)"+
+//                    "VALUES (?,?)";
 
-        Phone_book phone_book = new Phone_book();
+//            pstmnt.setInt(1,post_id);
+//            pstmnt.setInt(2,user_id);
 
-    //    phone_book.addLike(2,2);
- //       phone_book.addUsers("Sergei","Sergeev","http://vk5");
-        System.out.println( phone_book.getlikePost());
+//          int insertedRow = pstmnt.executeUpdate();
 
-        System.out.println(phone_book.getUsersPosts());
- //       phone_book.addPosts("New phone its very good", new Date(5-2-2024),new Time(16-33),6);
-        phone_book.deleteString(20);
-    }
-}
+//           var pstmnt = connection.prepareStatement(addLikes, Statement.RETURN_GENERATED_KEYS);
+
+//            while (rsPst.next()) {
+//                    int value = 1;
+//                    int post = rsPst.getInt("post_id");
+//                    int userLike = rsPst.getInt("user_id");
+//                    int author = rsPst.getInt("author_name");
+//                    int liker = rsPst.getInt("liker_name");
+
